a7bf00c9d58000236e32df1575212da61cc8d404
Austen Apigo <austenapigo@gmail.com>
2020-10-14 23:39:58 -0700

    dat

---
+++ R/example_data.R
@@ -0,0 +1,24 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(quad_rarefied)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Apigo and Oono 2020. 
+#'
+#' @source Apigo and Oono 2020
+#'
+#' @examples
+#' data(quad_rarefied)
+#' 
+#' \donttest{structural.specificity(quad_rarefied)}
+"quad_rarefied"
